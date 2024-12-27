1e5d3d669397d085963e24eddf8b1dd48ed6acf6
daniprieto <dani101val@gmail.com>
2024-12-28 00:45:53 +0100

    Added database integration when the app is running

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from db_setup import initialize_database
-from db_test import test_insert_data, test_query_data
-from db_operations import clear_database
+from db_test import test_insert_data
+from ui import HabitTracker
+from PyQt5.QtWidgets import QApplication
+import sys
 
 if __name__ == "__main__":
     initialize_database()
     test_insert_data()
 
-    test_query_data()
-    clear_database()
+    app = QApplication(sys.argv)
+    window = HabitTracker()
+    window.show()
+    sys.exit(app.exec_())
+    
